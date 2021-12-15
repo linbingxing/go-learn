eb1d8b4a0eb1add9e9a54f3a9f3ffac86a29a01a
linbingxing <linbingxing@zuipin.cn>
2021-12-15 18:40:08 +0800

    基础

---
+++ src/language/constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	const ui = 123456
+
+	const uf = 3.141592
+
+	fmt.Println(ui)
+	fmt.Println(uf)
+
+}
---
+++ src/language/variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var v1 int
+
+	v1 = 10
+
+	fmt.Println(v1)
+}
